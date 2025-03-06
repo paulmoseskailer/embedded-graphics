5448ebee009c026c0a053cc44b478d10f367482d
Paul Kailer <paulkailer@outlook.de>
2025-03-06 16:19:07 +0100

    fix visibility of prelude use statements

--- core/src/prelude.rs
+++ core/src/prelude.rs
@@ -1,15 +1,9 @@
 //! Prelude
-#[maybe_async_cfg::maybe(
-    idents(Drawable, ImageDrawable),
-    sync(feature = "draw_target_sync"),
-    async(
-        feature = "draw_target_async",
-        idents(DrawTarget(async = "DrawTargetAsync"))
-    )
-)]
-pub use crate::{draw_target::DrawTarget, drawable::Drawable, image::ImageDrawable};
+#[cfg(feature = "async_draw_target")]
+pub use crate::draw_target::DrawTargetAsync;
 #[doc(no_inline)]
 pub use crate::{
+    draw_target::DrawTarget,
     drawable::Pixel,
     geometry::{Dimensions, OriginDimensions, Point, Size},
     pixelcolor::{
@@ -18,3 +12,9 @@ pub use crate::{
     },
     primitives::PointsIter,
 };
+#[maybe_async_cfg::maybe(
+    idents(Drawable, ImageDrawable),
+    sync(feature = "draw_target_sync"),
+    async(feature = "draw_target_async")
+)]
+pub use crate::{drawable::Drawable, image::ImageDrawable};
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,34 +1,10 @@
 //! Prelude
 
-#[maybe_async_cfg::maybe(
-    idents(DrawTargetExt),
-    sync(feature = "draw_target_sync"),
-    async(
-        feature = "draw_target_async",
-        idents(DrawTarget(async = "DrawTargetAsync"))
-    )
-)]
-use crate::draw_target::{DrawTarget, DrawTargetExt};
-#[maybe_async_cfg::maybe(
-    idents(ImageDrawable),
-    sync(feature = "draw_target_sync"),
-    async(feature = "draw_target_async")
-)]
-use crate::image::ImageDrawable;
-#[maybe_async_cfg::maybe(
-    idents(PixelIteratorExt),
-    sync(feature = "draw_target_sync"),
-    async(feature = "draw_target_async")
-)]
-use crate::iterator::PixelIteratorExt;
-#[maybe_async_cfg::maybe(
-    idents(Drawable),
-    sync(feature = "draw_target_sync"),
-    async(feature = "draw_target_async")
-)]
-use crate::Drawable;
 #[doc(no_inline)]
+#[cfg(feature = "draw_target_async")]
+use crate::draw_target::DrawTargetAsync;
 pub use crate::{
+    draw_target::DrawTarget,
     geometry::{Angle, AngleUnit, Dimensions, OriginDimensions, Point, Size},
     image::ImageDrawableExt,
     iterator::ContiguousIteratorExt,
@@ -39,4 +15,18 @@ pub use crate::{
     primitives::{ContainsPoint, OffsetOutline, PointsIter, Primitive},
     transform::Transform,
     Pixel,
+};
+#[maybe_async_cfg::maybe(
+    idents(DrawTargetExt),
+    idents(PixelIteratorExt),
+    idents(ImageDrawable),
+    idents(Drawable),
+    sync(feature = "draw_target_sync"),
+    async(
+        feature = "draw_target_async",
+        idents(DrawTarget(async = "DrawTargetAsync"))
+    )
+)]
+pub use crate::{
+    draw_target::DrawTargetExt, image::ImageDrawable, iterator::PixelIteratorExt, Drawable,
 };
